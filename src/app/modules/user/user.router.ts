c14b69f4c3fc092022945ec6eb0939e288586c1e
rifatjahan823 <rifatjahan823@gmail.com>
2024-01-08 14:05:36 +0600

    complete get-single-user

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -5,6 +5,7 @@ const router=express.Router();
 
 router.post('/',UserController.createUser);
 router.get('/',UserController.getAllUser);
+router.get('/:id',UserController.getSingleUser);
 
 
 export const UserRouter=router
